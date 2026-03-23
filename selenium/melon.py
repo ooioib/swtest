211f39b124f237c3e62c966ea8e7e9fe1fa6fcfc
KIMYERIN <krin02@naver.com>
2026-03-23 14:47:52 +0900

    feat : 멜론 브라우저 실행 기능 구현, 주석 수정

--- selenium/melon.py
+++ selenium/melon.py
@@ -1,4 +1,4 @@
-# selenium/wait.py
+# selenium/melon.py
 
 
 # 브라우저를 실행 및 제어하기 위한 클래스
@@ -19,29 +19,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 # 대기 조건을 제공하는 클래스
 from selenium.webdriver.support import expected_conditions as EC    
 
+
 opts = Options()                                # 크롬 옵션 객체 생성
 opts.add_experimental_option("detach", True)    # 브라우저를 닫아도 창이 유지
-# opts.add_argument("--headless=new")           # 브라우저 백그라운드 실행
-opts.add_argument("--window-size=1280, 900")    # 창 크기 설정
+opts.add_argument("--window-size=1280, 900")    # 브라우저 백그라운드 실행
+
 
 driver = webdriver.Chrome(options = opts)    # 옵션 적용 후 크롬 브라우저 실행
 wait = WebDriverWait(driver, 10)             # 최대 10초 동안 조건을 만족할 때까지 대기
 
 try:
-    driver.get("https://www.python.org")  # 해당 URL로 이동 (웹페이지 열기)
-    wait.until(lambda d : d.title != "")  # 페이지 제목이 공백이 아님
-    print(f"Title: {driver.title}")       # 페이지 제목 출력
+    driver.get("https://www.melon.com/")     # 해당 URL로 이동 (웹페이지 열기)
+    wait.until(lambda d : d.title != "")     # 페이지 제목이 공백이 아님
+    print(f"Title: {driver.title}")          # 페이지 제목 출력
 
-#   elem = driver.find_element(By.ID, "id-search-field") # ID로 요소 찾기
-    elem = wait.until(EC.visibility_of_element_located(
-        (By.ID, "id-search-field")))    # 검색 입력창이 화면에 보일 때까지 대기 후 요소 가져오기
-    elem.clear()                        # 입력 필드 초기화
-    elem.send_keys("list")              # 검색어 입력
-    elem.send_keys(Keys.ENTER)          # 엔터키 입력(검색 실행)
+# 검색 입력창이 화면에 보일 때까지 대기 후 요소 가져오기
+    elem = wait.until(
+        EC.visibility_of_element_located((By.ID, "top_search")))
 
-# 검색 후, Search Python.org가 나올 때까지 대기
-    wait.until(EC.text_to_be_present_in_element(
-        (By.TAG_NAME, "h2"), "Search Python.org"))
+    elem.clear()                    # 입력 필드 초기화
+    elem.send_keys("bts")           # 검색어 입력
+    elem.send_keys(Keys.ENTER)      # 엔터키 입력(검색 실행)
 
 finally:
 #    driver.quit()      # 브라우저 종료
